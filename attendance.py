7d12a65cb2b7ab25994848380dd10f2a49f34385
suchithrakuttiyat <suchithrakuttiyat@gmail.com>
2019-08-07 08:07:41 +0200

    first draft

--- attendance.py
+++ attendance.py
@@ -1,15 +1,42 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Aug  6 12:54:29 2019
-
-@author: suchithrakv
-"""
-
+import sys
 import csv
-with open('attendance.csv','w',newline='')as f:
-    fieldnames=['column1','column2','column3']
-    thewriter=csv.DictWriter(f,fieldnames=fieldnames)
-    thewriter.writeheader()
-    thewriter.writerow({'column1':'one','column2':'two','column3':'three'})
+import datetime
+import os.path
+from os import path
+
+def createDb(filename):
+  print("Creating %s" %(filename))
+  with open(filename, "w") as csvFile:
+    fileWriter = csv.writer(csvFile, delimiter=',')
+    fileWriter.writerow(['Name'])
+    fileWriter.writerow(['Student1'])
+    fileWriter.writerow(['Student2'])
+    fileWriter.writerow(['Student3'])
+
+def updateDb(filename, name):
+  attendance=raw_input("Enter attendance, type P for present and type A for absent:")
+  print "Attendance sheet updated!"
+
+def query(dbFile, name):
+  found = False 
+  if found:
+    print("Query %s in %s" %(name, dbFile))
+  return found 
+
+def updateAttendance(name):
+  filename = "attendance.csv"
+  if (False == path.exists(filename)):
+    createDb(filename)
+  if (query(filename, name)):
+    updateDb(filename, name)
+  else:
+    print("Query on %s failed!" %(filename))
+
+def main():
+  print "This python file keep track of attendence of students in class room!"
+  name = raw_input("Enter your name:")
+  updateAttendance(name)
+
+if __name__ == "__main__":
+  main()
     
